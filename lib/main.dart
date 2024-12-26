80e741ba6d775eaa13e939cd24ad6f3d7f889251
daracoo <dkuzmanoski21@gmail.com>
2024-12-26 23:16:52 +0100

    Added some information in the card

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,58 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: const Text('Ninja ID Card')
+        title: const Text('Ninja ID Card',
+        style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'Chun-Li',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                    fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,)
+          ]
+        ),
       ),
     );
   }
